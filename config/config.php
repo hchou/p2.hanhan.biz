47433ebf99984cf3e24c5ea8a141e0b4c595f311
Hans H Chou <hchou@fas.harvard.edu>
2013-10-19 15:39:01 -0400

    Modifying config.php to specify DB name

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'Blubber');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -31,7 +31,8 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	//define('DB_NAME', "myapp_com"); ### default sample
+        define('DB_NAME', "p2_hanhan_biz"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
