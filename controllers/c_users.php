fb1e9bf2bf4bfa3d487076e44d0537a52205fd21
Hans H Chou <hchou@fas.harvard.edu>
2013-10-24 19:49:53 -0400

    Adding the logout function.

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -89,10 +89,30 @@ class users_controller extends base_controller {
             setcookie("token", $token, strtotime('+1 year'), '/');
 
             # Send them to the main page - or whever you want them to go
-            Router::redirect("/users/hello_world");
+            Router::redirect("/");
         }
     }
 
+    public function logout() {
+
+        # Generate and save a new token for next login
+        $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+
+        # Create the data array we'll use with the update method
+        # In this case, we're only updating one field, so our array only has one entry
+        $data = Array("token" => $new_token);
+
+        # Do the Database update
+        DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+
+        # Delete their token cookie by setting it to a date in the past - effectively logging them out
+        setcookie("token", "", strtotime('-1 year'), '/');
+
+        # Send them back to the main index.
+        Router::redirect("/");
+
+    }
+    
     public function profile() {
 
         # If user is blank, they're not logged in; redirect them to the login page
