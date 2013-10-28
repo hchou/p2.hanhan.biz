09fdddc8875d51b9d8ba4374de32c3c89b405dba
Hans H Chou <hchou@fas.harvard.edu>
2013-10-27 22:38:24 -0400

    Adding error-handling code.

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,14 +1,19 @@
 <form method='POST' action='/users/p_login'>
 
     Email<br>
-    <input type='text' name='email'>
-
+    <input type='text' name='email'>    
     <br><br>
 
     Password<br>
     <input type='password' name='password'>
-
     <br><br>
+
+    <?php if(isset($error)): ?>
+        <div class='error'>
+            Login failed. <?php echo $error ?>.
+        </div>
+        <br>
+    <?php endif; ?>
 
     <input type='submit' value='Log in'>
 
