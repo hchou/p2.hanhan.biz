ad69099c3ed22329a5b7dc4171a240a156f0ce42
Hans H Chou <hchou@fas.harvard.edu>
2013-10-28 23:18:06 -0400

    Showing the Delete column for Admin/Super users.

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,19 +1,32 @@
 <h1>Users</h1>
 
-<?php foreach($users as $user): ?>
+<table border="1">
+    <tr>
+        <th>First Name</th><th>Last Name</th><th>Follow</th>
+        <?php if ($user->role < 2): ?>
+            <th>Delete</th>
+        <?php endif; ?>
+    </tr>
+    <?php foreach($users_list as $my_user): ?>
+    <tr>
+        <!-- Print this user's name -->
+        <td><?=$my_user['first_name'] ?></td>
+        <td><?=$my_user['last_name'] ?></td>
 
-    <!-- Print this user's name -->
-    <?=$user['first_name']?> <?=$user['last_name']?>
+        <!-- If there exists a connection with this user, show a unfollow link -->
+        <?php if(isset($connections[$my_user['user_id']])): ?>
+            <td><a href='/posts/unfollow/<?=$my_user['user_id'] ?>'>Unfollow</a></td>
+            
+        <!-- Otherwise, show the follow link -->
+        <?php else: ?>
+            <td><a href='/posts/follow/<?=$my_user['user_id'] ?>'>Follow</a></td>
+        <?php endif; ?>
+        
+        <!-- If logged in user is an administrator, show a delete link -->
+        <?php if ($user->role < 2): ?>
+            <td><a href='/users/delete/<?=$my_user['user_id'] ?>'>Delete</a></td>
+        <?php endif; ?>
+    </tr>
+    <?php endforeach; ?>
+</table>
 
-    <!-- If there exists a connection with this user, show a unfollow link -->
-    <?php if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
-
-    <!-- Otherwise, show the follow link -->
-    <?php else: ?>
-        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-    <?php endif; ?>
-
-    <br><br>
-
-<?php endforeach; ?>
