098910a03508f15361a6b53af650498b54064c93
Hans H Chou <hchou@fas.harvard.edu>
2013-10-28 21:46:01 -0400

    Showing posts from following users only, adding Follow and Unfollow features.

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -41,30 +41,36 @@ class posts_controller extends base_controller {
     }
     
     public function index() {
-
+    
         # Set up the View
         $this->template->content = View::instance('v_posts_index');
-        $this->template->title   = "Posts";
-
-        # Build the query
-        $q = "SELECT 
-                posts .* , 
-                users.first_name, 
+        $this->template->title   = "All Posts";
+    
+        # Query
+        $q = 'SELECT 
+                posts.content,
+                posts.created,
+                posts.user_id AS post_user_id,
+                users_users.user_id AS follower_id,
+                users.first_name,
                 users.last_name
             FROM posts
+            INNER JOIN users_users 
+                ON posts.user_id = users_users.user_id_followed
             INNER JOIN users 
-                ON posts.user_id = users.user_id";
-
-        # Run the query
+                ON posts.user_id = users.user_id
+            WHERE users_users.user_id = '.$this->user->user_id;
+    
+        # Run the query, store the results in the variable $posts
         $posts = DB::instance(DB_NAME)->select_rows($q);
     
         # Pass data to the View
         $this->template->content->posts = $posts;
-
+    
         # Render the View
         echo $this->template;
-
-    }    
+    
+    }
     
     public function users() {
 
@@ -100,5 +106,33 @@ class posts_controller extends base_controller {
         echo $this->template;
     
     }
+    
+    public function follow($user_id_followed) {
+    
+        # Prepare the data array to be inserted
+        $data = Array(
+            "created" => Time::now(),
+            "user_id" => $this->user->user_id,
+            "user_id_followed" => $user_id_followed
+            );
+    
+        # Do the insert
+        DB::instance(DB_NAME)->insert('users_users', $data);
+    
+        # Send them back
+        Router::redirect("/posts/users");
+    
+    }
+    
+    public function unfollow($user_id_followed) {
+    
+        # Delete this connection
+        $where_condition = 'WHERE user_id = '.$this->user->user_id.' AND user_id_followed = '.$user_id_followed;
+        DB::instance(DB_NAME)->delete('users_users', $where_condition);
+    
+        # Send them back
+        Router::redirect("/posts/users");
+    
+    }    
     
 }
