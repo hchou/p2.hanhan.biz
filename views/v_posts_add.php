36841b59c1ad25cf605ced73ef185c98c54a3c6c
Hans H Chou <hchou@fas.harvard.edu>
2013-10-31 14:19:08 -0400

    Updating the view to be more consistent with other form pages.

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,9 +1,13 @@
-<form method='POST' action='/posts/p_add'>
+<link rel="stylesheet" type="text/css" href="/css/blubber_form.css">
 
-    <label for='content'>New Post:</label><br>
-    <textarea name='content' id='content'></textarea>
+<h1>Add a New Post</h1>
 
-    <br><br>
-    <input type='submit' value='New post'>
+<form class='blubberForm' method='POST' action='/posts/p_add'>
+    <fieldset>
+        <label for='content'>New Post:</label><br>
+        <textarea name='content' id='content'></textarea>
 
+        <br><br>
+        <input type='submit' value='New post'>
+    </fieldset>
 </form> 
