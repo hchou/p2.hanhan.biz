083ee223f334db9af2d2d45eab072adfe9092476
Hans H Chou <hchou@fas.harvard.edu>
2013-10-31 14:35:08 -0400

    Updating bullets.

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -3,15 +3,21 @@
 <ul>
 	
     <li><h4>User Profile Update</h4></li>
-    <dd>Verify 'current password' is correct before proceeding</dd>
-    <dd>Verify 'new email' isn't already in DB</dd>
-    <dd>Update only modified user information</dd>
-	
+    <ul>
+         <li>Verify 'current password' is correct before proceeding</li>
+        <li>Verify 'new email' isn't already in DB</li>
+        <li>Update only modified user information</li>
+    </ul>
+    
     <li><h4>User Delete</h4></li>
-    <dd>Admin/Superuser are allowed to delete other users</dd>
-    <dd>User cannot her/himself</dd>
+    <ul>
+        <li>Admin/Superuser are allowed to delete other users</li>
+        <li>User cannot her/himself</li>
+    </ul>
 
     <li><h4>Delete Posts</h4></li>
-    <dd>Admin/Superuser are allowed to delete posts</dd>   
+    <ul>
+        <li>Admin/Superuser are allowed to delete posts</li>   
+    </ul>
     
 </ul>
