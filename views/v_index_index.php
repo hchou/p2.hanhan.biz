36ccc2d18ebc3fcf8bdb65b4d126e5475075cdd6
Hans H Chou <hchou@fas.harvard.edu>
2013-10-31 15:27:20 -0400

    Updating unordered lists.

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -2,20 +2,20 @@
 <h3>+1 Features</h3>
 <ul>
 	
-    <li><h4>User Profile Update</h4></li>
+    <h4>User Profile Update</h4>
     <ul>
          <li>Verify 'current password' is correct before proceeding</li>
         <li>Verify 'new email' isn't already in DB</li>
         <li>Update only modified user information</li>
     </ul>
     
-    <li><h4>User Delete</h4></li>
+    <h4>User Delete</h4>
     <ul>
         <li>Admin/Superuser are allowed to delete other users</li>
         <li>User cannot her/himself</li>
     </ul>
 
-    <li><h4>Delete Posts</h4></li>
+    <h4>Delete Posts</h4>
     <ul>
         <li>Admin/Superuser are allowed to delete posts</li>   
     </ul>
