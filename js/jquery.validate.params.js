e617d5161882b1c36bf09c37b29d8c93d4dcbde2
Hans H Chou <hchou@fas.harvard.edu>
2013-10-30 22:44:46 -0400

    Adding validation for the Login page.

--- js/jquery.validate.params.js
+++ js/jquery.validate.params.js
@@ -1,27 +1,6 @@
-//First Name<br>
-//<input type='text' name='first_name'>
-//<br><br>
-//
-//Last Name<br>
-//<input type='text' name='last_name'>
-//<br><br>
-//
-//Email<br>
-//<input type='text' name='email' value=''>
-//<br><br>
-//
-//Password<br>
-//<input type='password' name='password' value=''>
-
-$.validator.setDefaults({
-    submitHandler: function() {
-        alert("submitted!");
-    }
-});
 
 $().ready(function() {
 
-    // validate signup form on keyup and submit
     $("#signupForm").validate({
         rules: {
             first_name: {
@@ -40,17 +19,11 @@ $().ready(function() {
                 equalTo: "#password",
                 required: true,
                 minlength: 5,
-
             },
             email: {
                 required: true,
                 email: true,
             },
-            //topic: {
-            //	required: "#newsletter:checked",
-            //	minlength: 2
-            //},
-            //agree: "required"
         },
         messages: {
             first_name: {
@@ -71,28 +44,25 @@ $().ready(function() {
                 equalTo: "Please enter the same password as above",
             },
             email: "Please enter a valid email address",
-            //agree: "Please accept our policy"
         }
     });
-
-    // propose username by combining first- and lastname
-    $("#username").focus(function() {
-        var firstname = $("#firstname").val();
-        var lastname = $("#lastname").val();
-        if (firstname && lastname && !this.value) {
-            this.value = firstname + "." + lastname;
+    
+    $("#loginForm").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            password: {
+                required: true,
+            },
+        },
+        messages: {
+            email: "Please enter your e-mail address",
+            password: {
+                required: "Please enter your password",
+            },
         }
     });
-
-    //code to hide topic selection, disable for demo
-    var newsletter = $("#newsletter");
-    // newsletter topics are optional, hide at first
-    var inital = newsletter.is(":checked");
-    var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-    var topicInputs = topics.find("input").attr("disabled", !inital);
-    // show when newsletter is checked
-    newsletter.click(function() {
-        topics[this.checked ? "removeClass" : "addClass"]("gray");
-        topicInputs.attr("disabled", !this.checked);
-    });
+    
 });
